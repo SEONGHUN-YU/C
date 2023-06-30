22fae62c5fe53774724c0746b7d76a19f5ab4606
SEONGHUN-YU <121845879+SEONGHUN-YU@users.noreply.github.com>
2023-07-01 02:42:30 +0900

    Add files via upload

---
+++ Chapter10/Chapter10_1/ObjectRelationship.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	// 객체들의 관계는
+	// 구성(요소), 집합, 제휴(연계), 의존, 상속 5개가 있다
+	// Composition, Aggregation, Association, Dependency, Inheritance
+	// part-of, has-a, uses-a, depends-on, is-a
+
+	return 0;
+}
